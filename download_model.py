4d3af3d9bca5b474aaf853a849008ddd5ac1b2e1
Jules Viennot <jules.viennot@gmail.com>
2025-04-14 13:08:25 +0200

    Small fix

--- download_model.py
+++ download_model.py
@@ -1,13 +1,15 @@
 import json
 import torch
-from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import AutoModelForCausalLM
 
 def main():
     with open("conf/config.json", "r") as config:
         cfg = json.load(config)
 
-    AutoModelForCausalLM.from_pretrained(
+    model = AutoModelForCausalLM.from_pretrained(
         cfg.model,
         torch_dtype=torch.bfloat16,
         device_map=None
     ).to("cpu")
+
+    model.save_pretrained("./models")
